a89d77627269f006c3f62426ce6578190239ebd7
Gert-Jan Paulissen <gert.jan.paulissen@gmail.com>
2020-11-14 16:55:04 +0100

    Added analyze

--- cmpdir.pl
+++ cmpdir.pl
@@ -1,16 +1,22 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl # -*- coding: utf-8 -*-
 
 =pod
 
 =head1 NAME
 
-cmpdir.pl - Compare the files in one or more directories (recursive). 
+cmpdir.pl - Compare the files in one or more directories (recursive) or analyze the compare STDOUT output. 
 
 =head1 SYNOPSIS
 
-  cmpdir.pl [OPTION...] DIRECTORY...
+  cmpdir.pl [GLOBAL OPTION...] compare [compare OPTION...] DIRECTORY...
+
+or
+
+  cmpdir.pl [GLOBAL OPTION...] analyze [analyze OPTION...] FILE...
 
 =head1 DESCRIPTION
+
+=head2 Compare directories
 
 First Use Case is to compare two iTunes Libraries.
 
@@ -40,7 +46,35 @@ In local strftime %Y-%m-%d %H:%M:%S format.
 
 =back
 
+=head2 Analyze the compare results
+
+The second Use Case is to compare the output of one or more comparisons.
+
+Every file in the standard output from the compare subcommand is gathered
+based on size. If the number of files is not equal to the number of
+directories or if not every file is equal the files are shown.
+
 =head1 OPTIONS
+
+=head2 GLOBAL OPTIONS
+
+=over 4
+
+=item B<--help>
+
+This help.
+
+=item B<--unit-test>
+
+Perform a unit test.
+
+=item B<--verbose>
+
+Increase verbosity level.
+
+=back
+
+=head2 compare OPTIONS
 
 =over 4
 
@@ -52,13 +86,15 @@ The number of bytes to read for the hash check. Defaults to 512.
 
 The hash function to use (hash or crc32). Defaults to hash from the internal Perl module B.
 
-=item B<--help>
+=back
 
-This help.
+=head2 analyze OPTIONS
 
-=item B<--verbose>
+=over 4
 
-Increase verbosity level.
+=item B<--threshold>
+
+Only display differences when the size is at least this number. Defaults to 0.
 
 =back
 
@@ -124,6 +160,7 @@ use File::Find::Rule;
 use Getopt::Long;
 use Pod::Usage;
 use POSIX qw(strftime);
+use Test::More; # do not know how many in advance
 
 use lib &dirname($0); # to find File::Copy::Recursive in this directory
 use MyFile;
@@ -145,7 +182,10 @@ my @descr = ( '==             ',
 
 my $bytes = 512;
 my $r_hash_func = 'hash';
+my $unit_test = 0;
 my $verbose = 0;
+my $subcmd = undef;
+my $threshold = 0;
 
 # FORMATS
 
@@ -186,11 +226,17 @@ $descr, $cmp, $cached
 
 sub main ();
 sub process_command_line ();
+sub compare ();
+sub analyze ($);
+sub print_analysis ($$);
+sub trim ($);
+sub prepare ();
 sub process ();
 sub log (@);
 sub quote ($);
 sub by_cmp (;$$);
 sub crc32 ($;$$);
+sub unit_test ();
 
 # MAIN
 
@@ -201,8 +247,16 @@ main();
 sub main () 
 {
     process_command_line();
-    prepare();
-    process();
+
+    if ($unit_test) {
+        unit_test();
+    } elsif ($subcmd eq 'compare') {
+        compare();
+    } elsif ($subcmd eq 'analyze') {
+        my ($rh_folders, $rh_files) = analyze('ARGV');
+        
+        print_analysis($rh_folders, $rh_files);
+    }
 }
 
 sub process_command_line ()
@@ -213,35 +267,177 @@ sub process_command_line ()
     if ( @ARGV == 1 && $ARGV[0] =~ s/^\s+//o ) {
         @ARGV = split( / /, $ARGV[0] );
     }
-    
-    Getopt::Long::Configure(qw(require_order));
+
+    # permute for subcommand handling (opposite of require_order
+    Getopt::Long::Configure(qw(permute));
 
     #
     GetOptions('help' => sub { pod2usage(-verbose => 2) },
-               'bytes=i' => \$bytes,
-               'function=s' => \$r_hash_func,
-               'verbose+' => \$verbose
-        )
+               'unit-test' => \$unit_test,
+               'verbose+' => \$verbose,
+               '<>' => sub {
+                   my($arg) = @_;
+
+                   if ($arg =~ m/^-/) {
+                       die "Usage error: unhandled option $arg detected in global option section";
+                   } elsif ($arg !~ m/^(compare|analyze)$/) {
+                       die "usage error: invalid subcommand $arg";
+                   } else {
+                       $subcmd = $arg;
+                       die '!FINISH'; # stop processing arguments 
+                   }
+               })
         or pod2usage(-verbose => 0);
 
-    pod2usage(-message => "$0: Bytes ($bytes) must be at least 1. Run with --help option.\n")
-        unless defined($bytes) && $bytes >= 1;
+    pod2usage(-verbose => 0)
+        unless ($unit_test or defined($subcmd));
 
-    my %hash_func = ( 'hash' => \&hash, 'crc32' => \&crc32 );
-    my @hash_func = sort keys %hash_func;
+    if (defined($subcmd) && $subcmd eq 'compare') {
+        GetOptions('bytes=i' => \$bytes,
+                   'function=s' => \$r_hash_func)
+            or pod2usage(-verbose => 0);
 
-    pod2usage(-message => "$0: Must supply a valid hash function (@hash_func). Run with --help option.\n")
-        unless exists $hash_func{$r_hash_func};
+        pod2usage(-message => "$0: Bytes ($bytes) must be at least 1. Run with --help option.\n")
+            unless defined($bytes) && $bytes >= 1;
 
-    $r_hash_func = $hash_func{$r_hash_func};
+        my %hash_func = ( 'hash' => \&hash, 'crc32' => \&crc32 );
+        my @hash_func = sort keys %hash_func;
+
+        pod2usage(-message => "$0: Must supply a valid hash function (@hash_func). Run with --help option.\n")
+            unless exists $hash_func{$r_hash_func};
+        
+        $r_hash_func = $hash_func{$r_hash_func};
     
-    pod2usage(-message => "$0: Must supply at least one directory. Run with --help option.\n")
-        unless @ARGV >= 1;
+        pod2usage(-message => "$0: Must supply at least one directory. Run with --help option.\n")
+            unless @ARGV >= 1;
 
-    foreach my $dir (@ARGV) {
-        pod2usage(-message => "$0: $dir is not a directory. Run with --help option.\n")
-            unless -d $dir;
+        foreach my $dir (@ARGV) {
+            pod2usage(-message => "$0: $dir is not a directory. Run with --help option.\n")
+                unless -d $dir;
+        }
+    } elsif (defined($subcmd) && $subcmd eq 'analyze') {
+        GetOptions('threshold=i' => \$threshold)
+            or pod2usage(-verbose => 0);
+
+        pod2usage(-message => "$0: Threshold ($threshold) must be at least 0. Run with --help option.\n")
+            unless defined($threshold) && $threshold >= 0;
+
+        foreach my $file (@ARGV) {
+            pod2usage(-message => "$0: $file is not a file. Run with --help option.\n")
+                unless -f $file;
+        }
     }
+}
+
+sub compare () {
+    prepare();
+    process();
+}
+    
+sub analyze ($) {
+    my $fh = shift @_;
+    my @pos = ();
+    my $part = undef;
+    my $prev_size;
+    # return values
+    my (@folders, %folders, %files);
+    
+    while (my $line = <$fh>){
+        chomp $line;
+
+        if ($line eq 'Nr  Origin') {
+            ;
+        } elsif ($line eq '--  ------') {
+            @pos = ( [0, 2], [4, undef] );
+            @folders = ();
+            $part = 1;
+        } elsif ($line eq 'Eq           Size  Origin  Modification time    Filename') {
+            ;
+        } elsif ($line eq '--           ----  ------  -----------------    --------') {
+            @pos = ( [0, 2], [4, 13], [19, 6], [27, 19], [48, undef] );            
+            $part = 2;
+        } elsif ($line eq 'Compare                  Count  Cached') {
+            ;
+        } elsif ($line eq '-------                  -----  ------') {
+            @pos = ();        
+        } elsif (scalar(@pos) > 0&& length($line) > 0) {
+            &log("line:", $line);
+            
+            my @cols = ();
+
+            foreach my $pos (@pos) {
+                my ($offset, $length) = ( $pos->[0], $pos->[1] );
+
+                push(@cols, trim( defined($length) ? substr($line, $offset, $length) : substr($line, $offset) ));
+            }
+
+            if ($verbose > 0) {
+                $line = '';
+            
+                for my $i (0..$#cols) {
+                    $line .= "[$i] '$cols[$i]' ";
+                }
+                
+                &log("pos:", $line);
+            }
+
+            if ($part == 1) {
+                # folders
+                $folders[$cols[0]] = $cols[1];
+                $folders{$cols[1]}++;
+            } else {
+                my ($equal, $size, $folder, $filename) = (($cols[0] eq '=='), $cols[1], $folders[$cols[2]], $cols[4]);
+
+                if ($size eq '') {
+                    $size = $prev_size;
+                }
+
+                if ($size >= $threshold) {
+                    # files
+                    if (!exists($files{$size}{files})) {
+                        $files{$size}{files} = [ "\n$folder/$filename" ];
+                        $files{$size}{equal} = 1;
+                    } else {
+                        push(@{$files{$size}{files}}, "\n$folder/$filename");
+                        $files{$size}{equal} += $equal;
+                    }
+                
+                    &log("files for $size:", "@{$files{$size}{files}}");
+                }
+                
+                $prev_size = $size;
+            }
+        }
+    }
+
+    return (\%folders, \%files);
+}
+
+sub print_analysis ($$) {
+    my ($rh_folders, $rh_files) = (@_);
+    
+    my $folder_count = scalar(keys %$rh_folders);
+
+    print STDOUT "\nNumber of folders: $folder_count\n";
+
+    foreach my $size (sort { int($b) <=> int($a) } keys %{$rh_files}) {
+        my $rh = $rh_files->{$size};
+        
+        if ($size >= $threshold) {
+            my $file_count = scalar(@{$rh->{files}});
+            
+            print STDOUT "\nNumber of files for size $size not $folder_count or not all equal: ", "@{$rh->{files}}\n" 
+                if ($file_count != $folder_count || $rh->{equal} != $folder_count);
+        }
+    }
+}
+
+sub trim ($) {
+    my $str = shift @_;
+
+    $str =~ s/^\s+|\s+$//g;
+
+    return $str;
 }
 
 sub prepare ()
@@ -428,3 +624,84 @@ sub crc32 ($;$$) {
 
     return $crc;
 }
+
+sub unit_test () {
+    $threshold = 9;
+    
+    my ($rh_folders, $rh_files) = analyze('DATA');
+
+    plan tests => 2 + 2 * scalar(keys %$rh_files);
+
+    my $rh;
+
+    ok( keys %$rh_folders == 2, 'number of folders should be 2' );
+    
+    ok( keys %$rh_files == 15, 'number of files should be 15' ); # threshold of 9
+
+    foreach my $size (sort { int($b) <=> int($a) } keys %$rh_files) {
+        my $rh = $rh_files->{$size};
+        my $file_count_act = scalar(@{$rh->{files}});
+        my $file_count_exp;
+
+        if ( $size =~ m/^(92583948|92331344|50316|5858|4485|1193|1047)$/ ) {
+            $file_count_exp = 1;
+        } else {
+            $file_count_exp = 2;
+        }
+
+        ok( $file_count_act == $file_count_exp, "number of files with size $size ($file_count_act) should be $file_count_exp" );
+
+        my $equal_act = $rh->{equal};
+        my $equal_exp = ( $size =~ m/^(32768|16384|82)$/ ? $file_count_act - 1 : $file_count_act );
+
+        ok( $equal_act == $equal_exp, "number of equal files with size $size ($equal_act) should be $equal_exp" );
+    }
+
+    done_testing();
+}
+
+# music.txt
+
+__DATA__
+
+
+Nr  Origin
+--  ------
+1   /Volumes/AirPort Disk/Apple Music
+2   /Volumes/iTunes/Music
+
+Eq           Size  Origin  Modification time    Filename
+--           ----  ------  -----------------    --------
+         92583948       1  2020-11-01 22:00:38  Music/Johnny Hallyday/Bercy 90/2-06 Aimer vivre (Live à Bercy _ 1990).m4a
+         92331344       2  2018-09-30 11:46:44  Music/Media.localized/Music/Johnny Hallyday/Bercy 90/2-06 Aimer vivre (Live à Bercy _ 1990).m4a
+         83094530       1  2014-10-12 13:32:30  Music/Led Zeppelin/Celebration Day [Live] [Disc 1]/1-04 In My Time Of Dying.m4a
+==                      2  2014-10-12 13:32:30  Music/Media.localized/Music/Led Zeppelin/Celebration Day [Live] [Disc 1]/1-04 In My Time Of Dying.m4a
+         79516345       1  2014-10-12 13:32:48  Music/Led Zeppelin/Celebration Day [Live] [Disc 2]/2-02 Dazed And Confused.m4a
+==                      2  2014-10-12 13:32:48  Music/Media.localized/Music/Led Zeppelin/Celebration Day [Live] [Disc 2]/2-02 Dazed And Confused.m4a
+         75972781       1  2014-10-12 13:35:12  Music/Led Zeppelin/Led Zeppelin Remasters [Disc 2]/2-09 Achilles Last Stand.m4a
+==                      2  2014-10-12 13:35:12  Music/Media.localized/Music/Led Zeppelin/Led Zeppelin Remasters [Disc 2]/2-09 Achilles Last Stand.m4a
+         75455261       1  2014-10-12 13:46:34  Music/Scorpions/Tokyo tapes/09 Fly to the rainbow.m4a
+==                      2  2014-10-12 13:46:34  Music/Media.localized/Music/Scorpions/Tokyo tapes/09 Fly to the rainbow.m4a
+         74990074       1  2014-10-08 22:14:42  Music/Blue Oyster Cult/Extraterrestial/1-07 Roadhouse blues.m4a
+==                      2  2014-10-08 22:14:42  Music/Media.localized/Blue Oyster Cult/Extraterrestial/1-07 Roadhouse blues.m4a
+            50316       2  2020-10-26 15:48:50  Music/iTunes Music Library.xml
+            32768       2  2020-09-27 15:34:17  Music/Music Library.musiclibrary/Genius.itdb
+                        1  2020-11-01 10:58:21  Music/Music Library.musiclibrary/Genius.itdb
+            16384       2  2020-10-26 12:30:00  Music/Music Library.musiclibrary/Extras.itdb
+                        1  2020-11-01 21:48:01  Music/Music Library.musiclibrary/Extras.itdb
+             5858       1  2020-11-01 22:14:35  Music/Music Library.musiclibrary/Application.musicdb
+             4485       2  2020-10-26 12:29:32  Music/Music Library.musiclibrary/Application.musicdb
+             1193       1  2020-11-01 22:06:20  Music/Music Library.musiclibrary/Library Preferences.musicdb
+             1047       2  2020-10-26 09:24:21  Music/Music Library.musiclibrary/Library Preferences.musicdb
+               82       2  2020-10-26 09:24:42  Music/Music Library.musiclibrary/Preferences.plist
+                        1  2020-11-01 21:47:22  Music/Music Library.musiclibrary/Preferences.plist
+                8       1  2020-11-01 22:14:36  Music/Music Library.musiclibrary/sentinel
+                        2  2020-10-26 15:42:54  Music/Music Library.musiclibrary/sentinel
+
+Compare                  Count  Cached
+-------                  -----  ------
+==                        4408    2204
+!= size                      0       0
+!= hash                    122      42
+!= file compare              0       0
+
